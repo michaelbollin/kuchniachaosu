cf709121337cdf5b806e08565ef83b0606a685c1
michaelbollin <michael@dcny.co>
2024-05-26 15:10:00 +0200

    feat: Add support for multiple images in post schema

--- schemas/post.ts
+++ schemas/post.ts
@@ -81,6 +81,38 @@ export default defineType({
       },
     }),
     defineField({
+      name: 'image2',
+      title: 'Image 2',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'image3',
+      title: 'Image 3',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'image4',
+      title: 'Image 4',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'image5',
+      title: 'Image 5',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
       name: 'date',
       title: 'Date',
       type: 'datetime',
