284a81dc722e3acde40998bdaa28017590706fbd
michaelbollin <michael@dcny.co>
2024-10-31 00:49:35 +0100

    📝 (package-lock.json): add "@vercel/analytics" package with version "^1.3.2" and its dependencies   📝 (package-lock.json): add "server-only" package with version "0.0.1" and its integrity   📝 (package.json): update dependencies with "@vercel/analytics" to version "^1.3.2"   📝 (pages/_app.tsx): import and add the <Analytics /> component from "@vercel/analytics/react" to the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import 'tailwindcss/tailwind.css'
 import { VisualEditing } from '@sanity/visual-editing/next-pages-router'
 import { AppProps } from 'next/app'
 import dynamic from 'next/dynamic'
+import { Analytics } from "@vercel/analytics/react"
 
 export interface SharedPageProps {
   draftMode: boolean
@@ -20,6 +21,7 @@ export default function App({
     <>
       {draftMode ? (
         <PreviewProvider token={token}>
+          <Analytics />
           <Component {...pageProps} />
         </PreviewProvider>
       ) : (
