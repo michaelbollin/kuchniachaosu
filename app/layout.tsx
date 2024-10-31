ea6629c142da48c9b5a040a4f2acf33b848900d3
michaelbollin <michael@dcny.co>
2024-10-31 02:01:15 +0100

    ✨ (layout.tsx): Introduce 'next/script' component to load scripts asynchronously for performance improvement   📝 (layout.tsx): Add Simple Analytics script to track website traffic on all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import 'tailwindcss/tailwind.css'
+import Script from 'next/script'
 
 export default function RootLayout({
   children,
@@ -7,7 +8,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Script src="https://scripts.simpleanalyticscdn.com/latest.js" /> 
+      </body>
     </html>
   )
 }
