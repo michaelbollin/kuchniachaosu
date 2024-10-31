0ea8d2b8c047317e5fdecba087fa27b7d95a10c3
michaelbollin <michael@dcny.co>
2024-10-31 01:00:34 +0100

    📝 (pages/_app.tsx): Remove unnecessary import of "Analytics" component   📝 (pages/_document.tsx): Add "Analytics" component to the body for tracking purposes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import 'tailwindcss/tailwind.css'
 import { VisualEditing } from '@sanity/visual-editing/next-pages-router'
 import { AppProps } from 'next/app'
 import dynamic from 'next/dynamic'
-import { Analytics } from "@vercel/analytics/react"
 
 export interface SharedPageProps {
   draftMode: boolean
@@ -21,7 +20,6 @@ export default function App({
     <>
       {draftMode ? (
         <PreviewProvider token={token}>
-          <Analytics />
           <Component {...pageProps} />
         </PreviewProvider>
       ) : (
