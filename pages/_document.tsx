83e171b140fba7ba1803352bc360a58deda3d509
michaelbollin <michael@dcny.co>
2024-11-22 17:15:31 +0100

    ✨ feat(document): add Google Analytics tracking
    - integrate Google Analytics by adding gtag scripts in the head
    - ensure tracking is initialized after page interaction for improved analytics setup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,11 +4,27 @@ import Script from 'next/script'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-JDW5899F2E"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-JDW5899F2E');
+            `,
+          }}
+        />
+      </Head>
       <body className="bg-white text-black">
         <Main />
         <NextScript />
-        <Script src="https://scripts.simpleanalyticscdn.com/latest.js" /> 
       </body>
     </Html>
   )
